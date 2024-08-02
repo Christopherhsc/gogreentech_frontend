f753eb02bbd6dfe4f46d57f250fc36a7fc4df379
Christopher Christensen <christopher.hsc2520@gmail.com>
2024-08-02 10:10:58 +0200

    landing-page + desktop got a little touch design and logic

--- src/app/consumers/desktop/components/installers/installers-data.ts
+++ src/app/consumers/desktop/components/installers/installers-data.ts
@@ -1,20 +1,20 @@
 export class InstallersData {
-  static mainText: string = ``
+  static mainText: string = ``;
   static installerText: string[] = [
     'Du bliver partner til Gogreentech',
     'Vi betaler dig for at have henvist en ejendom (henvisningsprovision)',
-    'Når vi skal have installeret vores udstyr, så er det jeres virksomhed der skal udføre arbejdet'
+    'Når vi skal have installeret vores udstyr, så er det jeres virksomhed der skal udføre arbejdet',
   ];
 
   static ownerText: string[] = [
-    'De bidrager til den grønne omstilling',
-    'Der er ingen omkostninger forbundet med løsningen for ejendomsejeren',
-    'De vil modtage en leje indtægt pr. ampere de lejer ud til Gogreentech'
+    'Du bidrager til grøn omstilling og din ejendom værdiforøges',
+    'Du har ingen omkostninger forbundet med løsningen',
+    'Du modtager lejeindtægt pr. ampere du udlejer',
   ];
 
   static solutionsText: string[] = [
     'Løsninger Gogreentech bruger for at balancere nettet.',
     'Der findes flere løsninger, men det er vigtigt at de kan reagere hurtigt (inden for få sekunder), her er batteriløsninger, der kan stå udendørs, en del af løsningen. Vores løsninger',
-    'Alle vinder på det og samtidig understøtter vi Energinet’s ønske om balancering af elnettet.'
+    'Alle vinder på det og samtidig understøtter vi Energinet’s ønske om balancering af elnettet.',
   ];
 }
